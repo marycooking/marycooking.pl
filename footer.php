8c1abb238f0b505dbd0c9f31febf0d16f08775d7
Konrad Kruk <kontakt@konradk.pl>
2014-02-13 20:41:28 +0100

    Wordpress theme files

---
+++ footer.php
@@ -0,0 +1,10 @@
+	</div>
+	</div>
+	<footer id="stopka">
+		
+		<p><strong>Mary Cooking</strong> - Kontakt@marycooking.pl - Wszelkie prawa zastrzeżone. Wszystko to zjadłam sama.</p>
+		
+	</footer>
+	<?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mary cooking</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="bower_components/normalize-css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href='http://fonts.googleapis.com/css?family=Lato:400,700,400italic' rel='stylesheet' type='text/css'>
+	<?php wp_head(); ?>
+</head>
+<body>
+	<div id="kontener">
+		<header>
+			<div id="logo">
+				<h1><a href="#"><img src="marycooking.png"></a></h1>
+				<h2>Mam płytę indukcyjną to korzystam</h2>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="#">Mięso</a></li>
+					<li><a href="#">Japonia</a></li>
+					<li><a href="#">Słodkości</a></li>
+					<li><a href="#">Warzywa</a></li>
+				</ul>
+			</nav>
+			<div class="clear"></div>
+		</header>
+		<div id="kontenerSrodkowy">
