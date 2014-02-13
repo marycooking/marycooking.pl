492284ae1ea849e3f31007813bf34303d15931a0
Konrad Kruk <kontakt@konradk.pl>
2014-02-13 21:01:17 +0100

    worpdress loop

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/bower_components/normalize-css/normalize.css">
 	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
 	<link href='http://fonts.googleapis.com/css?family=Lato:400,700,400italic' rel='stylesheet' type='text/css'>
+	 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<?php wp_head(); ?>
 </head>
 <body>
@@ -13,7 +14,7 @@
 		<header>
 			<div id="logo">
 				<h1><a href="#"><img src="<?php bloginfo('template_directory'); ?>/marycooking.png"></a></h1>
-				<h2>Mam płytę indukcyjną to korzystam</h2>
+				<h2>Blog o prostym, ale pysznym jedzeniu</h2>
 			</div>
 			<nav>
 				<?php wp_nav_menu(); ?> 
--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php get_header(); ?>
 			<section id="listaArtykulow">
+				<?php if (have_posts()) : ?>
+				<?php while (have_posts()) : the_post(); ?>
 				<article>
-					
 						<header>
 							<a href="#"><img src="obrazek1.jpg"></a>
 							<div class="metaIkonki">
@@ -25,97 +26,29 @@
 						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 						cillum dolore eu fugiat nulla pariatur. <a href="#">Czytaj całość</a></p>
-						
-					
 				</article>
-				<article>
-					<a href="#">
-						<header>
-							<img src="obrazek2.jpg">
-							<div class="metaIkonki">
-								<div class="mataCzas metaIkonka">
-									<span>30</span> min
-								</div>
-								<div class="mataTrudnosc metaIkonka">
-									łatwe
-								</div>
-								<div class="mataKoszt metaIkonka">
-									<span>36</span> zł
-								</div>
-							</div>
-							<div class="clear"></div>
-							<p class="metaInformacje">22 lutego 2014</p>
-							<h1 class="tytul_artykulu">Japońskie kuleczki z ryżem i kapustą</h1>
-							
-						</header>
-						<p>Staropolska zupa przygotowywana przez młode panny dla swoich panów. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur. </p>
-					</a>
-				</article>
-				<article>
-					<a href="#">
-						<header>
-							<img src="obrazek1.jpg">
-							<div class="metaIkonki">
-								<div class="mataCzas metaIkonka">
-									<span>30</span> min
-								</div>
-								<div class="mataTrudnosc metaIkonka">
-									łatwe
-								</div>
-								<div class="mataKoszt metaIkonka">
-									<span>36</span> zł
-								</div>
-							</div>
-							<div class="clear"></div>
-							<p class="metaInformacje">22 lutego 2014</p>
-							<h1 class="tytul_artykulu">Zupa z mięsem i zieleniną</h1>
-							
-						</header>
-						<p>Staropolska zupa przygotowywana przez młode panny dla swoich panów. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur. </p>
-					</a>
-				</article>
-				<article>
-					<a href="#">
-						<header>
-							<img src="obrazek2.jpg">
-							<div class="metaIkonki">
-								<div class="mataCzas metaIkonka">
-									<span>30</span> min
-								</div>
-								<div class="mataTrudnosc metaIkonka">
-									łatwe
-								</div>
-								<div class="mataKoszt metaIkonka">
-									<span>36</span> zł
-								</div>
-							</div>
-							<div class="clear"></div>
-							<p class="metaInformacje">22 lutego 2014</p>
-							<h1 class="tytul_artykulu">Japońskie kuleczki z ryżem i kapustą</h1>
-							
-						</header>
-						<p>Staropolska zupa przygotowywana przez młode panny dla swoich panów. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur. </p>
-					</a>
-				</article>
+				<?php endwhile; ?>
+				<div class="navigation">
+				<div class="alignleft">
+				<?php posts_nav_link('','','&laquo; Poprzednie wpisy') ?>
+				</div>
+				<div class="alignright">
+				<?php posts_nav_link('','Następne wpisy &raquo;','') ?>
+				</div>
+				</div>
+				<?php else : ?>
+				<h2 class="center">Nie znaleziono</h2>
+				<p class="center"><?php _e("Niestety, nie znaleziono żądanych postów."); ?></p>
+				<?php endif; ?>
+				
 			</section>
 			<aside id="panelBoczny">
 				<form id="wyszukiwarka">
 					<input type="text" placeholder="wyszukaj">
 				</form>
 				<h3>O blogu i o mnie</h3>
-				<p>Nazywam się Piorun i lubię kotki. Na tym blogu będę opisywała swoje szalone przepisy. Lubię Japonię więc gotuję jak w Japonii. Lubię mięso więc gotuję mięso. Warzywa mi smakują więc również je gotuję. Słodycze są słodkie i pyszne.</p>
+				<p>Nazywam się Marysia i bardzo lubię jeść. Od kilku lat lubię też gotować. Okazało się, że inni lubią z kolei jeść to, co ugotowałam. I o tym jest ten blog.</p>
+				<p>Piszę o Japonii - moim ulubionym kraju z najlepszym i najzdrowszym jedzeniem na świecie. O słodyczach, bo one wychodzą mi chyba najlepiej. O czasie, którego często brakuje w kuchni, ale dzięki temu wpadają do głowy świetne pomysły i niebanalne rozwiązania.</p>
 				<h3>Najpopularniejsze przepisy</h3>
 				<ul id="najpopularniejszePrzepisy">
 					<li>
