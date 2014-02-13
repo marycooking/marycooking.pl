0fa8fc7b43093179cbed1831f1be32bb220293d8
Konrad Kruk <kontakt@konradk.pl>
2014-02-13 22:45:42 +0100

    storing post views fix

--- functions.php
+++ functions.php
@@ -3,4 +3,28 @@ add_theme_support( 'post-thumbnails' );
 add_image_size( 'duzy_waski_kwadrat', 630, 250, true ); 
 add_image_size( 'maly_waski_kwadrat', 315, 125, true ); 
 register_nav_menu( 'glowne', 'Menu główne' );
+function getPostViews($postID){
+    $count_key = 'post_views_count';
+    $count = get_post_meta($postID, $count_key, true);
+    if($count==''){
+        delete_post_meta($postID, $count_key);
+        add_post_meta($postID, $count_key, '0');
+        return "0 View";
+    }
+    return $count.' Views';
+}
+function setPostViews($postID) {
+    $count_key = 'post_views_count';
+    $count = get_post_meta($postID, $count_key, true);
+    if($count==''){
+        $count = 0;
+        delete_post_meta($postID, $count_key);
+        add_post_meta($postID, $count_key, '0');
+    }else{
+        $count++;
+        update_post_meta($postID, $count_key, $count);
+    }
+}
+// Remove issues with prefetching adding extra views
+remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
 ?>
