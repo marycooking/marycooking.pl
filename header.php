cb4023525c968bea330d3eadb780bb5c0465f47e
Konrad Kruk <kontakt@konradk.pl>
2014-02-13 20:48:12 +0100

    Includes fixes

--- header.php
+++ header.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
 <head>
-	<title>Mary cooking</title>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" type="text/css" href="bower_components/normalize-css/normalize.css">
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<title><?php wp_title(); ?></title>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>/bower_components/normalize-css/normalize.css">
+	 <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
 	<link href='http://fonts.googleapis.com/css?family=Lato:400,700,400italic' rel='stylesheet' type='text/css'>
 	<?php wp_head(); ?>
 </head>
